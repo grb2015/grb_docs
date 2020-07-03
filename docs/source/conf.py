8e4b31db777455c4bce09d67878590d284815cb2
grb2015 <949409706@qq.com>
2020-07-03 12:03:13 +0800

    [add] 更改文档主题 ref:https://sphinx-rtd-theme.readthedocs.io/en/latest/installing.html

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-
+import sphinx_rtd_theme
 # -- Project information -----------------------------------------------------
 
 project = 'grbDocsTest'
@@ -28,7 +28,10 @@ author = 'grb'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+     "sphinx_rtd_theme",
 ]
+
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
